f73125574fb87cebc91ac63b4dd2e2c24b84a1ea
porissawa <porissawa@users.noreply.github.com>
2019-01-06 14:17:09 -0200

    added menu toggle

--- frontend/src/config/store.js
+++ frontend/src/config/store.js
@@ -1,3 +1,21 @@
 import Vue from 'vue'
-import VueX from 'vuex'
+import Vuex from 'vuex'
 
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    //dados e funções de manipulação:
+
+    state: {
+        isMenuVisible: true
+    },
+    mutations: {
+        toggleMenu(state, isVisible) {
+            if (isVisible === undefined) {
+                state.isMenuVisible = !state.isMenuVisible
+            } else {
+                state.isMenuVisible = isVisible
+            }
+        }
+    }
+})
